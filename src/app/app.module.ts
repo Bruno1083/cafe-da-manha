552f433d189921b6d1864dab4da471148cab8099
Bruno1083 <cavalcante1083@gmail.com>
2021-10-10 19:38:35 -0300

    componente form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
@@ -6,18 +8,28 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormCadastroComponent } from './form-cadastro/form-cadastro.component';
+import { ListConvidadosComponent } from './list-convidados/list-convidados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormCadastroComponent
+    FormCadastroComponent,
+    ListConvidadosComponent
   ],
   imports: [
+  RouterModule.forRoot([
+    {path: '', component: FormCadastroComponent},
+    {path: 'list-convidados', component: ListConvidadosComponent},
+    {path: '', redirectTo: '/list-convidados', pathMatch: 'full'},
+  ]),
     MDBBootstrapModule.forRoot(),
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/list-convidados/list-convidados.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-convidados',
+  templateUrl: './list-convidados.component.html',
+})
+export class ListConvidadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
