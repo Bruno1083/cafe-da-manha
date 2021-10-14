fb27c4a8b82cdb6aa17fda7c435c0276934f45ed
Bruno1083 <cavalcante1083@gmail.com>
2021-10-14 16:12:57 -0300

    class service

--- src/app/services/convidados.service.ts
+++ src/app/services/convidados.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConvidadosService {
 
-  constructor(private http: HttpClientModule) { }
+  convidadosUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  list() {
+    return this.http.get<Array<any>>(this.convidadosUrl);
+  }
+
+  criate(convidado: any){
+    return this.http.post(this.convidadosUrl, convidado);
+  }
+
 }
